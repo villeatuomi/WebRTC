2ec635fc30dd91ea5adda146174b7566ca454c97
bukomp <edvard@shalaev.com>
2019-10-02 09:07:44 +0300

    little cleanup

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
 
 
@@ -16,14 +15,3 @@ const App = () => {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-/*
-
-        !!!!*** VILLE READ TEXT BELOW ***!!!!
-
-*/
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
