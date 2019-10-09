f35e18b0099ee7983a230b4e3e85a0868f2987d6
bukomp <edvard@shalaev.com>
2019-10-09 13:12:30 +0300

    minor "quality of life" changes

--- back/index.js
+++ back/index.js
@@ -2,17 +2,18 @@ const app = require('./app')
 const PORT = require('./config').port
 
 
-const server = require('http').createServer(app.app);
+const server = require('http').createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+});
 const secureServer = require('https').createServer(app.credentials, app.app)
-const io = require('socket.io')(server);
 const ioS = require('socket.io')(secureServer);
 
-require('./socket')(io)
 require('./socket')(ioS)
 
 
 server.listen(PORT.http, () => {
-  console.log(`Web server running on port: ${PORT.http}`)
+  console.log(`Web redirection server running on port: ${PORT.http}`)
 })
 
 secureServer.listen(PORT.https, () => {
